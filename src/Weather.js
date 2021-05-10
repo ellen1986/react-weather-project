47371b093356e61601a72ad08bfb755bb7c126f5
Ellen Nimako <ypokua@yahoo.com>
2021-05-10 12:37:04 +0100

    Forecast layout and Api  added

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherMore from "./WeatherMore";
+import WeatherForecast from "./WeatherForecast"
 import axios from "axios";
 import "./Weather.css";
 export default function Weather(props) {
@@ -8,6 +9,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
@@ -53,6 +55,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherMore data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
--- src/WeatherMore.js
+++ src/WeatherMore.js
@@ -3,38 +3,34 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
-
 export default function WeatherMore(props) {
-    return (
-        <div className="WeatherMore">
-            <h1>{props.data.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={props.data.date} />
-          </li>
-          <li className="text-capitalize">{props.data.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="clearfix">
-                <div className="float-left">
-                <WeatherIcon 
-                code={props.data.icon} 
-                alt={props.data.description} />
-              </div>
-              <div className="float-left">
-                  <WeatherTemperature celsius={props.data.temperature} />
-                
-              </div>
+  return (
+    <div className="WeatherMore">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
-          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
